50a09e23f4038c484293cdb5f920972406e5ceb1
Dali @ Linux <ross4han@gmail.com>
2023-06-15 19:09:03 +0100

    update

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run `hugo`
+echo "Running hugo"
+hugo
+sleep 1s
+
+# Add all changes with `git add -A`
+echo "Adding changes with git add"
+git add -A
+sleep 1s
+
+# Commit changes with `git commit -m "update"`
+echo "Committing changes with git commit"
+git commit -m "update"
+sleep 1s
+
+# Push changes with `git push`
+echo "Pushing changes with git push"
+git push
